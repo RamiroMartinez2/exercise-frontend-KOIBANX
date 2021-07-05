eff2e9dca6daf726abd084632d594ebe69697af7
LEN-PC0B7SP5\ramir <ramiro.rc.rm@gmail.com>
2021-07-05 18:58:13 -0300

    Added filter by id and / or cuit and / or trade

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-
+import "./App.css";
+import Filters from "./components/Filters";
 function App() {
   return (
     <div className="App">
-    
+      <Filters />
     </div>
   );
 }
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const baseURL = "https://api.koibanx.com/stores";
 
 const Filters = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [query, setQuery] = useState("");
 
-export default Filters
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const applySearch = () => {
+    const inputParams = `{"$or": [{"id": {"$regex" :"${query}"}}, {"cuit": {"$regex" :"${query}"}}, {"comercio": {"$regex" :"${query}"}}]}`;
+    const url = `${baseURL}?q=${inputParams}`;
+    console.log(url );
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      Buscar:
+      <input
+        type="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <br />
+      <button onClick={applySearch} disabled={!query}>
+      Aplicar busqueda
+      </button>
+    </form>
+  );
+};
+
+export default Filters;
