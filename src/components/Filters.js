46caee255e603b554ecaf576f0a9632249f2d16d
LEN-PC0B7SP5\ramir <ramiro.rc.rm@gmail.com>
2021-07-05 21:39:13 -0300

    Sort by cuit and trade

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,17 +5,30 @@ const baseURL = "https://api.koibanx.com/stores";
 const Filters = () => {
   const [query, setQuery] = useState("");
   const [onlyActive, setOnlyActive] = useState();
+  const [sortBy, setSortBy] = useState([]);
 
   const onSubmit = (e) => {
     e.preventDefault();
   };
 
+  const handleSortBy = (e) => {
+    const name = e.target.name;
+    const checked = e.target.checked;
+    if (checked) setSortBy((prev) => [...prev, name]);
+    if (!checked) setSortBy((prev) => prev?.filter((i) => i !== name));
+  };
+
   const applySearch = () => {
     const inputParams = `{"$or": [{"id": {"$regex" :"${query}"}}, {"cuit": {"$regex" :"${query}"}}, {"comercio": {"$regex" :"${query}"}}]}`;
     const activeParams = `,{"active": ${onlyActive}}`;
+    const sortBy0 = sortBy?.length > 0 ? sortBy?.[0] : "";
+    const sortBy1 = sortBy?.length > 1 ? sortBy?.[1] : "";
+    const sortByParams =
+      sortBy0 &&
+      `&h={"$orderby": {"${sortBy0}": 1${sortBy1 ? `, "${sortBy1}": 1` : ""}}}`;
     const url = `${baseURL}?q={"$and": [${inputParams}${
       onlyActive ? activeParams : ""
-    }]}`;
+    }]}${sortByParams}`;
     console.log(url);
   };
 
@@ -35,6 +48,22 @@ const Filters = () => {
           onChange={(e) => setOnlyActive(e.target.checked)}
         />
         Solo activos
+      </label>
+      <br />
+      Ordenar por:
+      <br />
+      <label style={{ cursor: "pointer" }}>
+        <input
+          type="checkbox"
+          name="comercios"
+          onChange={(e) => handleSortBy(e)}
+        />
+        Comercios
+      </label>
+      <br />
+      <label style={{ cursor: "pointer" }}>
+        <input type="checkbox" name="cuit" onChange={(e) => handleSortBy(e)} />
+        CUIT
       </label>
       <br />
       <button onClick={applySearch} disabled={!query}>
